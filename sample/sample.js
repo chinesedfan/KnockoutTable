fd04c47d1f0c2a90ac2416dfbb473efd562083f6
chinesedfan <chinesedfan@qq.com>
2015-05-25 00:56:08 +0800

    use url args to switch data

--- sample/sample.js
+++ sample/sample.js
@@ -1,7 +1,18 @@
-//var data = WC2014Data;
-var data = SimCityData;
+function getUrlParam(name, targetStr) {
+    var target = targetStr || window.location.search + window.location.hash,
+        reg = new RegExp('[\\?#&]' + name + '=([^\\?#&]*)(\\?|#|&|$)', 'i'),
+        matches = reg.exec(target);
+
+    return matches ? decodeURIComponent(matches[1]) : '';
+}
+
+var test = getUrlParam('test') || '0';
+var datas = {
+	"0": WC2014Data,
+	"1": SimCityData
+};
 
 var table = new KnockoutTable($('.J_container'), {
-	data: data
+	data: datas[test]
 });
 table.draw();
